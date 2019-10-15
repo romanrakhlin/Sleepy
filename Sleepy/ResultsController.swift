5a2dc20c549aa724d1a7929ce027c7c07c9f2259
romarakhlin <rrakhlin@gmail.com>
2019-10-16 01:24:44 +0300

    first commit

---
+++ Sleepy/ResultsController.swift
@@ -0,0 +1,30 @@
+//
+//  ResultsController.swift
+//  Sleepy
+//
+//  Created by Roman Rakhlin on 15.10.2019.
+//  Copyright © 2019 Roman Rakhlin. All rights reserved.
+//
+
+import UIKit
+
+class ResultsController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
