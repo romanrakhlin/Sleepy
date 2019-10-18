f19f8104b03353a69b95ed4126de35317db34386
romarakhlin <rrakhlin@gmail.com>
2019-10-18 07:35:21 +0300

    new logo

--- Sleepy/ViewController.swift
+++ Sleepy/ViewController.swift
@@ -343,7 +343,7 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         pickerView.dataSource = self
         
         button.layer.cornerRadius = 20
-        photo.layer.cornerRadius = 30
+        //photo.layer.cornerRadius = 30
         
         hoursField.inputView = pickerView
         minutesField.inputView = pickerView
